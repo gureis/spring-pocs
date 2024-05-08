a6520c2abc39d139038a581e6d59f0b479ec9b95
gureis <gustavo.reis@creditas.com>
2024-05-08 08:13:18 -0300

    ft: add filter to wrap request into a cached request
    Signed-off-by: gureis <gustavo.reis@creditas.com>

---
+++ src/main/kotlin/com/springpocs/findPropertyAnnotation/CachedRequestBodyFilter.kt
@@ -0,0 +1,62 @@
+package com.springpocs.findPropertyAnnotation
+
+import org.springframework.stereotype.Component
+import org.springframework.util.StreamUtils
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import javax.servlet.FilterChain
+import javax.servlet.ReadListener
+import javax.servlet.ServletInputStream
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletRequestWrapper
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class CachedRequestBodyFilter : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val cachedBodyHttpServletRequest = CachedBodyHttpServletRequest(request)
+        filterChain.doFilter(cachedBodyHttpServletRequest, response)
+    }
+}
+
+
+class CachedBodyHttpServletRequest(request: HttpServletRequest) : HttpServletRequestWrapper(request) {
+    private val cachedBody: ByteArray
+
+    init {
+        val requestInputStream: InputStream = request.inputStream
+        cachedBody = StreamUtils.copyToByteArray(requestInputStream)
+    }
+
+    override fun getInputStream(): ServletInputStream {
+        return CachedBodyServletInputStream(cachedBody)
+    }
+}
+
+
+class CachedBodyServletInputStream(cachedBody: ByteArray?) : ServletInputStream() {
+    private val cachedBodyInputStream: InputStream
+
+    init {
+        cachedBodyInputStream = ByteArrayInputStream(cachedBody)
+    }
+
+    override fun read(): Int {
+        return cachedBodyInputStream.read()
+    }
+
+    override fun isFinished(): Boolean {
+        return cachedBodyInputStream.available() == 0
+    }
+
+    override fun isReady(): Boolean {
+        return true
+    }
+
+    override fun setReadListener(listener: ReadListener?) {}
+}
